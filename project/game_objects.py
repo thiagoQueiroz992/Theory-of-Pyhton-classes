8fd45e41d70e75d9c70e38bab163a0056360178b
Thiago Queiroz <thsqueiroz.2k26@gmail.com>
2025-10-21 19:16:41 -0300

    Add of attributes
    Some attributes (characteristics) has been added to the parent classes.

--- project/game_objects.py
+++ project/game_objects.py
@@ -1,13 +1,23 @@
 class InanimatedObjects():
-    def __init__(self):
-        pass
+    def __init__(self, hsize: float, vsize: float):
+        self.hsize = hsize
+        self.vsize = vsize
 
 
 class PlayableCharacters():
-    def __init__(self):
-        pass
+    def __init__(self, name: str, hsize: float, vsize: float, health: int, damage: int, defense: int):
+        self.name = name
+        self.hsize = hsize
+        self.vsize = vsize
+        self.health = health
+        self.damage = damage
+        self.defense = defense
 
 
 class Creatures():
-    def __init__(self):
-        pass
+    def __init__(self, hsize: float, vsize: float, health: int, damage: int, defense: int):
+        self.hsize = hsize
+        self.vsize = vsize
+        self.health = health
+        self.damage = damage
+        self.defense = defense
