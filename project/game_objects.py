7d8be123c6cd43fc3666f34fdc15e33176565237
Thiago Queiroz <thsqueiroz.2k26@gmail.com>
2025-10-21 16:48:43 -0300

    Initial classes creation
    I'm creating the first classes to control game objects

---
+++ project/game_objects.py
@@ -0,0 +1,13 @@
+class InanimatedObjects():
+    def __init__(self):
+        pass
+
+
+class PlayableCharacters():
+    def __init__(self):
+        pass
+
+
+class Creatures():
+    def __init__(self):
+        pass
