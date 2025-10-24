0b2a017bd37b709887e2bd4c45bf0a5931c625bc
Thiago Queiroz <thsqueiroz.2k26@gmail.com>
2025-10-24 20:17:59 -0300

    Actions added
    Added some behavior functions to PlayableCharacter and Creatures classes

--- project/game_objects.py
+++ project/game_objects.py
@@ -13,6 +13,24 @@ class PlayableCharacters():
         self.damage = damage
         self.defense = defense
 
+    def walking(self, speed: int, dir = "straight"):
+        if dir == "straight":
+            print("Follow straight")
+        elif dir == "back":
+            print("Back")
+        elif dir == "left":
+            print("Turn left")
+        elif dir == "right":
+            print("Turn right")
+        else:
+            print("Error")
+    
+    def attacking(self):
+        print(f"You attacked it with {self.damage} of damage.")
+    
+    def taking_damage(self):
+        print(f"You took damage and now you're {self.health - 1} of health.")
+
 
 class Creatures():
     def __init__(self, hsize: float, vsize: float, health: int, damage: int, defense: int):
@@ -21,3 +39,21 @@ class Creatures():
         self.health = health
         self.damage = damage
         self.defense = defense
+    
+    def walking(self, speed: int, dir = "straight"):
+        if dir == "straight":
+            print("Follow straight")
+        elif dir == "back":
+            print("Back")
+        elif dir == "left":
+            print("Turn left")
+        elif dir == "right":
+            print("Turn right")
+        else:
+            print("Error")
+    
+    def attacking(self):
+        print(f"Enemy attacked.")
+    
+    def taking_damage(self):
+        print(f"Enemy took damage. Now its health is {self.health - 1}.")
